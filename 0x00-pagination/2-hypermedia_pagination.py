7189e57cd4a53c98948a207ea50a892084c3515e
Emelda-debug <teacayemmie@gmail.com>
2024-07-20 07:44:05 +0200

    d

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,25 +39,26 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """retrieves a page of data """
-        assert isinstance(page, type(page_size)) == int
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
+        start_idx, end_idx = index_range(page, page_size)
         data = self.dataset()
-        if start > len(data):
+        if start_idx > len(data):
             return []
-        return data[start:end]
+        return data[start_idx:end_idx]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Retrieves page information """
         data = self.get_page(page, page_size)
-        start, end = index_range(page, page_size)
+        start_idx, end_idx = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
         return {
             'page_size': len(data),
             'page': page,
             'data': data,
-            'next_page': page + 1 if end < len(self.__dataset) else None,
-            'prev_page': page - 1 if start > 0 else None,
+            'next_page': page + 1 if end_idx < len(self.__dataset) else None,
+            'prev_page': page - 1 if start_idx > 0 else None,
             'total_pages': total_pages
         }
