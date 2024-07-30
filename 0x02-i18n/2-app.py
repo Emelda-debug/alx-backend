2a6bf8f7cb777851dbbaaa513d6c94920a1c8e6f
Emelda-debug <teacayemmie@gmail.com>
2024-07-30 20:35:43 +0200

    tasks

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Task 2: Get locale from request """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """The Config class"""
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieving the locale for web page
+
+    Returns:
+        str: best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """default route to render html """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
